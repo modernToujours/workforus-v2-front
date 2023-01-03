7b3eab5048d4b7c560aee381a2784fbac24c460a
최영원 <youngwon0607@gmail.com>
2023-01-03 19:37:05 +0900

    chore : 더미 파일 삭제 및 mui  적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { EmotionCache } from "@emotion/cache";
+import createEmotionCache from "../src/lib/createEmotionCache";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CacheProvider } from "@emotion/react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../src/lib/theme";
+import { CssBaseline } from "@mui/material";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
