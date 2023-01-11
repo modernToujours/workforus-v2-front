36fb58ecca8d191a5976e0850009c6fb2fc50c08
최영원 <youngwon0607@gmail.com>
2023-01-11 23:31:56 +0900

    feature : 쿠키를 통한 로그인 추가

---
+++ src/components/auth/AuthProvider.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth } from '../../hooks/useAuth';
+import { getCookie } from 'cookies-next';
+import { decrypt } from '../../lib/encrypt';
+import { useAppDispatch } from '../../redux/hooks';
+import { login, logout } from '../../redux/auth/authSlice';
+
+type AuthProvierProps = {
+  children: React.ReactNode;
+};
+
+function AuthProvider({ children }: AuthProvierProps) {
+  const { id, name, roles, isLogin } = useAuth();
+  const dispatch = useAppDispatch();
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const encryptedToken = getCookie('token');
+    if (encryptedToken) {
+      const jwtToken = decrypt(encryptedToken?.toString());
+      setToken(jwtToken);
+      axios.defaults.headers.common['Authorization'] = token;
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .get(`${process.env.NEXT_PUBLIC_API_ADDRESS}/auth`)
+        .then((res) => {
+          console.log(res.data);
+          dispatch(
+            login({
+              id: res.data.login,
+              name: res.data.name,
+              role: res.data.roles,
+            }),
+          );
+        })
+        .catch((err) => {
+          dispatch(logout);
+        });
+    } else {
+      dispatch(logout);
+    }
+  }, [dispatch, token]);
+
+  return <>{children}</>;
+}
+
+export default AuthProvider;
